ec88692a6205bfcb6a524f8ed2a971b3371acb03
Miguel Martín <mmartinv@redhat.com>
2025-08-26 12:20:12 +0200

    fix: update CI after server split
    Signed-off-by: Miguel Martín <mmartinv@redhat.com>

--- .github/scripts/container-utils.sh
+++ .github/scripts/container-utils.sh
@@ -3,11 +3,14 @@
 set -xeuo pipefail
 shopt -s expand_aliases
 
-alias go-fdo-client="docker run --rm --volume /tmp/device-credentials:/tmp/device-credentials --network fdo --workdir /tmp/device-credentials go-fdo-client"
+creds_dir="/tmp/device-credentials"
+alias go-fdo-client="docker run --rm --volume "${creds_dir}:${creds_dir}" --network fdo --workdir ${creds_dir} go-fdo-client"
 
 source "$(dirname "${BASH_SOURCE[0]}")/fdo-utils.sh"
+
 
 # When the device onboarding is running in a container we need
 # to setup the RVIPAddress to the actual IP of the rendezvous
 # container as 127.0.0.1 won't work.
 rendezvous_ip="$(docker inspect --format='{{json .NetworkSettings.Networks}}' "rendezvous" | jq -r '.[]|.IPAddress')"
+owner_ip="$(docker inspect --format='{{json .NetworkSettings.Networks}}' "owner" | jq -r '.[]|.IPAddress')"
--- .github/scripts/fdo-utils.sh
+++ .github/scripts/fdo-utils.sh
@@ -2,17 +2,62 @@
 
 set -xeuo pipefail
 
-manufacturer_ip=127.0.0.1
-rendezvous_ip=127.0.0.1
-owner_ip=127.0.0.1
+creds_dir=/tmp/device-credentials
+device_credentials=${creds_dir}/creds.bin
+
+certs_dir=/tmp/certs
 
 manufacturer_dns=manufacturer
-rendezvous_dns=rendezvous
-owner_dns=owner
+manufacturer_ip=127.0.0.1
+manufacturer_port=8038
+manufacturer_key="${certs_dir}/manufacturer.key"
+manufacturer_crt="${manufacturer_key/\.key/.crt}"
+manufacturer_pub="${manufacturer_key/\.key/.pub}"
+device_ca_key="${certs_dir}/device-ca.key"
+device_ca_crt="${device_ca_key/\.key/.crt}"
+device_ca_pub="${device_ca_key/\.key/.pub}"
 
-manufacturer_service="${manufacturer_dns}:8038"
-rendezvous_service="${rendezvous_dns}:8041"
-owner_service="${owner_dns}:8043"
+rendezvous_dns=rendezvous
+rendezvous_ip=127.0.0.1
+rendezvous_port=8041
+
+owner_dns=owner
+owner_ip=127.0.0.1
+owner_port=8043
+owner_onboard_log="/tmp/onboarding-${owner_dns}.log"
+owner_ov="/tmp/owner.ov"
+owner_key="${certs_dir}/owner.key"
+owner_crt="${owner_key/\.key/.crt}"
+owner_pub="${owner_key/\.key/.pub}"
+
+manufacturer_service="${manufacturer_dns}:${manufacturer_port}"
+rendezvous_service="${rendezvous_dns}:${rendezvous_port}"
+owner_service="${owner_dns}:${owner_port}"
+
+generate_cert() {
+  local key=$1
+  local crt=$2
+  local pub=$3
+  local subj=$4
+  openssl ecparam -name prime256v1 -genkey -outform der -out "${key}"
+  openssl req -x509 -key "${key}" -keyform der -subj "${subj}" -days 365 -out "${crt}"
+  openssl x509 -in "${crt}" -pubkey -noout -out "${pub}"
+}
+
+generate_certs() {
+  mkdir -p "${certs_dir}"
+  generate_cert "${manufacturer_key}" "${manufacturer_crt}" "${manufacturer_pub}" "/C=US/O=FDO/CN=Manufacturer"
+  generate_cert "${device_ca_key}" "${device_ca_crt}" "${device_ca_pub}" "/C=US/O=FDO/CN=Device CA"
+  generate_cert "${owner_key}" "${owner_crt}" "${owner_pub}" "/C=US/O=FDO/CN=Owner"
+  ls -l "${certs_dir}"
+  chmod a+r "${certs_dir}"/*
+}
+
+get_server_logs() {
+  docker logs manufacturer
+  docker logs rendezvous
+  docker logs owner
+}
 
 setup_hostname() {
   local dns
@@ -26,17 +71,11 @@ setup_hostname() {
   fi
 }
 
-setup_hostnames () {
-  setup_hostname ${manufacturer_dns} ${manufacturer_ip}
-  setup_hostname ${rendezvous_dns} ${rendezvous_ip}
-  setup_hostname ${owner_dns} ${owner_ip}
-}
-
 wait_for_service() {
     local status
     local retry=0
     local -r interval=2
-    local -r max_retries=1005
+    local -r max_retries=5
     local service=$1
     echo "Waiting for ${service} to be healthy"
     while true; do
@@ -51,6 +90,12 @@ wait_for_service() {
     done
 }
 
+setup_hostnames () {
+  setup_hostname ${manufacturer_dns} ${manufacturer_ip}
+  setup_hostname ${rendezvous_dns} ${rendezvous_ip}
+  setup_hostname ${owner_dns} ${owner_ip}
+}
+
 wait_for_fdo_servers_ready () {
   # Manufacturer server
   wait_for_service "${manufacturer_service}"
@@ -61,46 +106,74 @@ wait_for_fdo_servers_ready () {
 }
 
 set_rendezvous_info () {
-    curl --fail --verbose --silent \
-         --header 'Content-Type: text/plain' \
-         --data-raw "[[[5,\"${rendezvous_dns}\"],[3,8041],[12,1],[2,\"${rendezvous_ip}\"],[4,8041]]]" \
-         "http://${manufacturer_service}/api/v1/rvinfo"
+  local manufacturer_service=$1
+  local rendezvous_dns=$2
+  local rendezvous_ip=$3
+  local rendezvous_port=$4
+  curl --fail --verbose --silent \
+       --header 'Content-Type: text/plain' \
+       --request POST \
+       --data-raw "[[[5,\"${rendezvous_dns}\"],[3,${rendezvous_port}],[12,1],[2,\"${rendezvous_ip}\"],[4,${rendezvous_port}]]]" \
+       "http://${manufacturer_service}/api/v1/rvinfo"
 }
 
 run_device_initialization() {
-  go-fdo-client --blob creds.bin --debug device-init "http://${manufacturer_service}" --device-info=gotest --key ec256
+  rm -rf "${creds_dir}"
+  mkdir -p "${creds_dir}"
+  cd ${creds_dir}
+  go-fdo-client --blob "${device_credentials}" --debug device-init "http://${manufacturer_service}" --device-info=gotest --key ec256
+  cd -
 }
 
-send_voucher_to_owner () {
-  local guid
-  guid=$(go-fdo-client --blob creds.bin --debug print | grep GUID | awk '{print $2}')
-  curl --fail --verbose --silent "http://${manufacturer_service}/api/v1/vouchers?guid=${guid}" -o ownervoucher
-  curl -X POST --fail --verbose --silent "http://${owner_service}/api/v1/owner/vouchers" --data-binary @ownervoucher
+get_device_guid () {
+  go-fdo-client --blob "${device_credentials}" --debug print | grep GUID | awk '{print $2}'
+}
+
+get_ov_from_manufacturer () {
+  local manufacturer_service=$1
+  local guid=$2
+  local output=$3
+  curl --fail --verbose --silent "http://${manufacturer_service}/api/v1/vouchers?guid=${guid}" -o "${output}"
+}
+
+set_owner_redirect_info () {
+  local service=$1
+  local ip=$2
+  local port=$3
+  curl --location --request POST "http://${service}/api/v1/owner/redirect" \
+       --header 'Content-Type: text/plain' \
+       --data-raw "[[\"${ip}\",\"${ip}\",${port},3]]"
+}
+
+send_ov_to_owner () {
+  local owner_service=$1
+  local output=$2
+  curl --fail --verbose --silent "http://${owner_service}/api/v1/owner/vouchers" --data-binary "@${output}"
 }
 
 run_to0 () {
-  local guid
-  guid=$(go-fdo-client --blob creds.bin --debug print | grep GUID | awk '{print $2}')
+  local owner_service=$1
+  local guid=$2
   curl --fail --verbose --silent "http://${owner_service}/api/v1/to0/${guid}"
 }
 
 run_fido_device_onboard () {
-  go-fdo-client --blob creds.bin --debug onboard --key ec256 --kex ECDH256 | tee onboarding.log
-  grep 'FIDO Device Onboard Complete' onboarding.log
-}
-
-get_server_logs() {
-  docker logs manufacturer
-  docker logs rendezvous
-  docker logs owner
+  local log=$1
+  cd ${creds_dir}
+  go-fdo-client --blob "${device_credentials}" --debug onboard --key ec256 --kex ECDH256 | tee "${log}"
+  cd -
+  grep 'FIDO Device Onboard Complete' "${log}"
 }
 
 test_onboarding () {
   setup_hostnames
   wait_for_fdo_servers_ready
-  set_rendezvous_info
+  set_rendezvous_info ${manufacturer_service} ${rendezvous_dns} ${rendezvous_ip} ${rendezvous_port}
   run_device_initialization
-  send_voucher_to_owner
-  run_to0
-  run_fido_device_onboard
+  guid=$(get_device_guid ${device_credentials})
+  get_ov_from_manufacturer ${manufacturer_service} "${guid}" ${owner_ov}
+  set_owner_redirect_info ${owner_service} ${owner_ip} ${owner_port}
+  send_ov_to_owner ${owner_service} ${owner_ov}
+  run_to0 ${owner_service} "${guid}"
+  run_fido_device_onboard ${owner_onboard_log}
 }
